a5b01fc55cc8d098333060c496ce4e788f9f3363
Janek Szynal <jan.szynal@gmail.com>
2014-12-18 21:53:56 +0100

    initial commit

--- RingOfFire/ViewController.swift
+++ RingOfFire/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        //bądź mi suką
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
