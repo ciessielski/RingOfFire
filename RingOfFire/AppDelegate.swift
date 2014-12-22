99cb9cf8da5a88c64ff36818579845b60d2ae0f1
Michał Ciesielski <ciessielski@gmail.com>
2014-12-22 22:20:52 +0100

    testing github-slack integration

--- RingOfFire/AppDelegate.swift
+++ RingOfFire/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 //halo tu charatonik
+//slack ?    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         return true
