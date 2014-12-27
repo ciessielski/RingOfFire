5217cca5e275efcb3e7db47b018fe791d09d7db4
Michał Ciesielski <ciessielski@gmail.com>
2014-12-27 20:32:30 +0100

    sample image and description

--- RingOfFire/InstructionViewController.swift
+++ RingOfFire/InstructionViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class InstructionViewController: UIViewController
+
 {
+ 
+    @IBOutlet weak var instructionTextView: UITextView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        instructionTextView.text = "Drink like a King - you must pour a little of your drink into the cup that is in the middle of the table. Whoever picks up the FORTH card must drink the whole cup, which could be filled with different drinks, so who knows how bad it could taste!";
     }
 
     override func didReceiveMemoryWarning()
