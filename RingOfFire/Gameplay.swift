94d413854d4791e6e118f818b02517e1c5f6d15a
Pumpkin K <evilpumpkin@MacBook-Air-Adam.local>
2014-12-30 03:39:26 +0100

    Dodałem klasę Gameplay

---
+++ RingOfFire/Gameplay.swift
@@ -0,0 +1,25 @@
+//
+//  GamePlay.swift
+//  RingOfFire
+//
+//  Created by Paweł Charatonik on 30.12.2014.
+//  Copyright (c) 2014 Michał Ciesielski. All rights reserved.
+//
+
+import Foundation
+
+class Gameplay {
+    
+    var deck: NSMutableArray
+    var cardOnTop: NSNumber
+
+    init(deck:NSMutableArray, cardOnTop:NSNumber)
+    {
+        self.deck=deck
+        self.cardOnTop=cardOnTop
+    }
+    
+    func resetGame(){
+    }
+}
+
