48232b27b969611e72f1839997825b71b529b8de
Pumpkin K <evilpumpkin@MacBook-Air-Adam.local>
2015-01-02 22:48:26 +0100

    new classes

---
+++ RingOfFire/Card.swift
@@ -0,0 +1,24 @@
+//
+//  Card.swift
+//  RingOfFire
+//
+//  Created by Paweł Charatonik on 02.01.2015.
+//  Copyright (c) 2015 Michał Ciesielski. All rights reserved.
+//
+
+import Foundation
+
+
+
+class Card{
+
+//    var type : NSString
+    var title : String?
+    var instructions : String?
+    var slug : String
+    
+    init(slug: String) {
+        self.slug = slug
+    }
+
+}
--- RingOfFire/Gameplay.swift
+++ RingOfFire/Gameplay.swift
@@ -10,15 +10,34 @@ import Foundation
 
 class Gameplay {
     
-    var deck: NSMutableArray
-    var cardOnTop: NSNumber
+    var deck: NSMutableArray?
+    var cardOnTop: NSNumber?
+    
+    var titlesDictionary = ["castle": "King of the Castle", "categories" : "Categories", "chicks" : "Chicks", "you" : "You Drink", "choose" : "You Choose", "dicks" : "Dicks", "mate" : "Pick a Mate", "heaven" : "Heaven", "fis" : "Fis & Bas", "story" : "Story", "snake" : "Snake Eyes", "king" : "Drink Like a King", "rhyme" : "Rhyme", "tale" : "Tale", "smoke" : "Smoke Break"]
 
-    init(deck:NSMutableArray, cardOnTop:NSNumber)
+     var instructionsDictionary = ["castle": "King of the Castle", "categories" : "Categories", "chicks" : "Chicks", "you" : "You Drink", "choose" : "You Choose", "dicks" : "Dicks", "mate" : "Pick a Mate", "heaven" : "Heaven", "fis" : "Fis & Bas", "story" : "Story", "snake" : "Snake Eyes", "king" : "Drink Like a King", "rhyme" : "Rhyme", "tale" : "Tale", "smoke" : "Smoke Break"]
+    
+    init()
     {
-        self.deck=deck
-        self.cardOnTop=cardOnTop
+        //double loop
+        for in1 in 0...2 {
+            
+        }
+        
+        //triple loop
+        
+        
+        //quadruple loop
+        
+        
     }
     
+    
+    
+    func shuffle(){
+    }
     func resetGame(){
     }
 }
+
+
