9197d8d1926449a735647bdedf6f435a5ea0fb20
Paweł Charatonik <p.charatonik@gmail.com>
2015-04-13 20:21:01 +0200

    alert bug fixed

--- RingOfFire/SettingsViewController.swift
+++ RingOfFire/SettingsViewController.swift
@@ -22,17 +22,25 @@ class SettingsViewController: UIViewController
 
     @IBAction func restartAction(sender: AnyObject)
     {
-        let alertController = UIAlertController(title: NSLocalizedString("Restart game", comment: "alert"), message:NSLocalizedString("Are you sure?", comment: "alertDetail") , preferredStyle: .Alert)
+        if objc_getClass("UIAlertController") != nil
+        {
+        
+            let alertController = UIAlertController(title: NSLocalizedString("Restart game", comment: "alert"), message:NSLocalizedString("Are you sure?", comment: "alertDetail") , preferredStyle: .Alert)
 
-        let cancelAction = UIAlertAction(title: NSLocalizedString("Nope", comment: "n"), style: .Cancel)
-        {(action) in}
-        alertController.addAction(cancelAction)
+            let cancelAction = UIAlertAction(title: NSLocalizedString("Nope", comment: "n"), style: .Cancel)
+                {(action) in}
+            alertController.addAction(cancelAction)
         
-        let OKAction = UIAlertAction(title: NSLocalizedString("Restart", comment: "r"), style: .Default)
-        {(action) in self.restartGame()}
-        alertController.addAction(OKAction)
+            let OKAction = UIAlertAction(title: NSLocalizedString("Restart", comment: "r"), style: .Default)
+                {(action) in self.restartGame()}
+            alertController.addAction(OKAction)
         
-        self.presentViewController(alertController, animated: true){}
+            self.presentViewController(alertController, animated: true){}
+        }
+        else
+        {
+            restartGame()
+        }
     }
     
     func restartGame()
